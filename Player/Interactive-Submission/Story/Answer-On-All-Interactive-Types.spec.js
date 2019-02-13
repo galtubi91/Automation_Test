f0a1c1135eaf13714a95b2cbf275e12a8d2f59c5
galtubi91 <gal.tubi@apester.com>
2019-02-13 16:57:24 +0200

    work in progress

--- Player/Interactive-Submission/Story/Answer-On-All-Interactive-Types.spec.js
+++ Player/Interactive-Submission/Story/Answer-On-All-Interactive-Types.spec.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 
 let browser;
 let pages;
-const testUnit = "https://galtubi91.github.io/articles/Poll-stg/";
+const testUnit = "https://galtubi91.github.io/articles/Story-stg/";
 let apesterIframe;
 const playerCrashWithException = [];
 
@@ -32,7 +32,7 @@ beforeAll(async () => {
     });
 });
 
-test("Default Layout", async () => {
+test("Interactive Poll", async () => {
     await pages[0].waitFor('#main > article > div.entry-wrapper > div > div.apester-media.apester-element');
     await pages[0].hover('#main > article > div.entry-wrapper > div > div.apester-media.apester-element');
     apesterIframe = await pages[0].frames()[1];
@@ -49,9 +49,9 @@ test("Default Layout", async () => {
         );
         return Array.from(elements).map(element => element.innerText);
     });
-    expect(nextSlideTitle[1]).toBe("big image");
+    expect(nextSlideTitle[1]).toBe("Quiz");
 });
-test("Big image", async () => {
+test("Interactive Quiz", async () => {
     let bigImageOptions = await apesterIframe.$$(
         "div[class^=StyledInteractiveMultiChoice] > div"
     );
@@ -64,24 +64,10 @@ test("Big image", async () => {
         );
         return Array.from(elements).map(element => element.innerText);
     });
-    expect(nextSlideTitle[1]).toBe("versus");
+    expect(nextSlideTitle[1]).toBe("Interactive Image");
 });
-test("Versus", async () => {
-    let versusOptions = await apesterIframe.$$(
-        "div[class^=StyledInteractiveMultiChoice] > div"
-    );
-    await versusOptions[0].click();
-    await pages[0].waitFor(5000);
-    expect(playerCrashWithException.length).toEqual(0);
-    let nextSlideTitle = await apesterIframe.evaluate(() => {
-        const elements = document.querySelectorAll(
-            "div[class^=StyledCanvasTextComponentV1] > p"
-        );
-        return Array.from(elements).map(element => element.innerText);
-    });
-    expect(nextSlideTitle[1]).toBe("image in focus");
-});
-test("Image in focus ", async () => {
+
+test("Interactive Image", async () => {
     let imageInFocusOptions = await apesterIframe.$$(
         "div[class^=StyledInteractiveMultiChoice] > div"
     );
